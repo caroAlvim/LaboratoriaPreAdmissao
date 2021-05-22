ca6c2420a2f209188ae7b532bb525d8eddde89b7
Carolina Alvim <caro.alvim@gmail.com>
2021-05-22 20:11:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ let nome = prompt("Qual é o seu nome?") // vai perguntar o nome na tela
 document.write("Olá, " + nome)
 
 let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar ok ou cancel
-  if ((iniciar == 1)) {
-    var sim = alert("Então vamos começar!");
-
+  if (iniciar == "1") {
+    let sim = alert("Então vamos começar!");
+   
+    listaCorretas = new Array{
+    };
+    
+    listaErradas = new Array{
+    };
+    
     let primeira = prompt("Pergunta 1 \n Qual é o nome do ator que interpreta Lúcifer na série da Netflix? \n A - Tom Ellis \n B - Tom Riddle \n C - Tom Fords") 
       if ((primeira == "a" )){
-        let correta1 = document.getElementById("primeira")  
-        document.innerHtml("Resposta 1 correta")
+        primeira = document.write("Resposta 1 correta");
+         listaCorretas.push(primeira);
+        //let correta1 = document.getElementById("primeira");  
+        //let respostaUm = document.innerHtml("Resposta 1 correta");
       } else {
-        document.innerHtml("Resposta 1 errada")
+        primeira = document.innerHtml("Resposta 1 errada");
+        listaErradas.push(primeira);
       }
        
    //console.log(correta1.innerHTML) 
@@ -21,28 +30,27 @@ let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar
 
     let segunda = prompt("Pergunta 2 \n Existem quantos protagonistas na série Friends? \n a - 1 \n b - 2 \n c - 6 ") 
      if ((segunda == "c")){
-      document.write ("Resposta 2 correta")
+      
+      segunda = document.write ("Resposta 2 correta");
+       listaCorretas.push(segunda);
      } else {
-      document.write ("Resposta 2 errada")
+      segunda = document.write ("Resposta 2 errada")
+       listaErradas.push(segunda);
      }
     
 
     let terceira = window.prompt("Pergunta 3 \n Em que ano foi lançada a série Game of Thrones? \n a - 2010 \n b - 2008 \n c - 2020")
-     if ((terceira == "b")){
-      document.write ("correta")
+     if (terceira == "b"){
+      terceira = document.write ("Resposta 3 correta")
+       listaCorretas.push(terceira);
+       
      } else {
-      document.write ("errada")
+      terceira = document.write ("Resposta 3 errada");
+       listaErradas.push(terceira);
      }
     
-
-  
-    
-    
-     if ((terceira == "b")){
-      document.write ("Resposta 3 correta")
-     } else {
-      document.write ("Resposta 3 errada")
-     }
+    document.write(listaCorretas, listaErradas);
+ 
     
     else  {
     var nao = window.alert( "Ah, q pena... Até logo!");
