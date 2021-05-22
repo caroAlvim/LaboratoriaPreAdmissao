605222c420e0679552f1651aa942a57067ae055e
Carolina Alvim <caro.alvim@gmail.com>
2021-05-22 20:29:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ document.write("Olá, " + nome)
 
 let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar ok ou cancel
   if (iniciar == "1") {
-    let sim = alert("Então vamos começar!");
+    const sim = alert("Então vamos começar!");
    
-    listaCorretas = new Array{
-    };
+    const listaCorretas = new Array(
+    );
     
-    listaErradas = new Array{
-    };
+    const listaErradas = new Array(
+    );
     
     let primeira = prompt("Pergunta 1 \n Qual é o nome do ator que interpreta Lúcifer na série da Netflix? \n A - Tom Ellis \n B - Tom Riddle \n C - Tom Fords") 
-      if ((primeira == "a" )){
+      if ((primeira == "a" || "A" )){
         primeira = document.write("Resposta 1 correta");
          listaCorretas.push(primeira);
         //let correta1 = document.getElementById("primeira");  
@@ -29,7 +29,7 @@ let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar
         //document.write ()
 
     let segunda = prompt("Pergunta 2 \n Existem quantos protagonistas na série Friends? \n a - 1 \n b - 2 \n c - 6 ") 
-     if ((segunda == "c")){
+     if ((segunda == "c"|| "C")){
       
       segunda = document.write ("Resposta 2 correta");
        listaCorretas.push(segunda);
@@ -40,7 +40,7 @@ let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar
     
 
     let terceira = window.prompt("Pergunta 3 \n Em que ano foi lançada a série Game of Thrones? \n a - 2010 \n b - 2008 \n c - 2020")
-     if (terceira == "b"){
+     if (terceira == "b" || "B"){
       terceira = document.write ("Resposta 3 correta")
        listaCorretas.push(terceira);
        
