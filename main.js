cef892796f10f9f7d8b3150dcff2c2ad79aa037c
Carolina Alvim <caro.alvim@gmail.com>
2021-12-04 14:15:16 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,93 +1,61 @@
+const nome = prompt("Qual é o seu nome?")
+const saudacao = document.getElementById("hello");
 
-let nome = prompt("Qual é o seu nome?") // vai perguntar o nome na tela
-document.write("Olá, " + nome)
+saudacao.innerHTML = `<p> Olá, ${nome}!<\p>`
 
-let iniciar = prompt("Deseja inciar a prova?\n 1 Sim \n 2 Não") //vai perguntar ok ou cancel
-  if (iniciar == "1") {
-    const sim = alert("Então vamos começar!");
-   
-    const listaCorretas = [];
-    
-    const listaErradas = [];
-    
-    let primeira = prompt("Pergunta 1 \n Qual é o nome do ator que interpreta Lúcifer na série da Netflix? \n A - Tom Ellis \n B - Tom Riddle \n C - Tom Fords") 
-      if ((primeira == "a" || "A" )){
-        primeira = document.write("Resposta 1 correta");
-         listaCorretas.push(primeira);
-        //let correta1 = document.getElementById("primeira");  
-        //let respostaUm = document.innerHtml("Resposta 1 correta");
+function initGame() {
+  let iniciar = prompt("Deseja inciar o jogo? \n 1-Sim \n 2-Não")
+    if (iniciar == "1") {
+      alert("Então vamos começar!");
+      startGame()
+    } else {
+      alert("Ok! Até logo," + nome );
+    }
+}
+
+// Inicia o jogo
+function startGame(){
+  // const listaCorretas = [];   
+  // const listaErradas = [];
+
+  const primeira = prompt("Pergunta 1 \n Qual é o nome do ator que interpreta Lúcifer na série da Netflix? \n A - Tom Ellis \n B - Tom Riddle \n C - Tom Ford") 
+      if(primeira === "a") {
+        // listaCorretas.push("Resposta 1")
+        console.log("primeira")
+        document.getElementById("resp_corretasUm").innerHTML += `<p>Resposta 1 <\p>`;
       } else {
-        primeira = document.innerHtml("Resposta 1 errada");
-        listaErradas.push(primeira);
-      }
-       
-   //console.log(correta1.innerHTML) 
+        //const erradaUm = "Resposta 1"
+        // listaErradas.push("Resposta 1")
+        document.getElementById("resp_erradasUm").innerHTML += `<p>Resposta 1 <\p>`;
+      };
 
-     //else if ((primeira !== A))
-        //document.write ()
+  const segunda = prompt("Pergunta 2 \n Existem quantos protagonistas na série Friends? \n A - 3 \n B - 2 \n C - 6 ") 
+      if(segunda === "c"){
+        // listaCorretas.push("Resposta 2");
+        document.getElementById("resp_corretasDois").innerHTML += `<p>Resposta 2<\p>`;
+      } else {
+        //const erradaDois = "Resposta 2"
+        document.getElementById("resp_erradasDois").innerHTML += `<p>Resposta 2 <\p>`;
+      }; 
 
-    let segunda = prompt("Pergunta 2 \n Existem quantos protagonistas na série Friends? \n a - 1 \n b - 2 \n c - 6 ") 
-     if ((segunda == "c"|| "C")){
-      
-      segunda = document.write ("Resposta 2 correta");
-       listaCorretas.push(segunda);
-     } else {
-      segunda = document.write ("Resposta 2 errada")
-       listaErradas.push(segunda);
-     }
+  const terceira = prompt("Pergunta 3 \n Em que ano foi lançada a série Game of Thrones? \n A - 2010 \n B - 2008 \n C - 2020")
+      if(terceira === "b"){
+        //const respTres = "Resposta 3"
+        //listaCorretas.push("Resposta 3"); 
+        document.getElementById("resp_corretasTres").innerHTML += `<p> Resposta 3<\p>`;
+      } else {
+        //const erradaTres = "Resposta 3"
+        //listaErradas.push("Resposta 3");
+        document.getElementById("resp_erradasTres").innerHTML += `<p> Resposta 3 <\p>`;
+      };
+      //apresentar resultado final - acertos e erros
+    // const respErradas = document.getElementById("resp_erradas");
+    // const respCorretas = document.getElementById("resp_corretas");
+
+    // respErradas.innerHTML = `<div> ${listaErradas}<\div>`;
+    // respCorretas.innerHTML = `<div> ${listaCorretas}<\div>`;
+}     
     
+  initGame()   
 
-    let terceira = window.prompt("Pergunta 3 \n Em que ano foi lançada a série Game of Thrones? \n a - 2010 \n b - 2008 \n c - 2020")
-     if (terceira == "b" || "B"){
-      terceira = document.write ("Resposta 3 correta")
-       listaCorretas.push(terceira);
-       
-     } else {
-      terceira = document.write ("Resposta 3 errada");
-       listaErradas.push(terceira);
-     }
-    
-    document.write(listaCorretas, listaErradas);
  
-    
-    else  {
-    var nao = window.alert( "Ah, q pena... Até logo!");
-  }
-
-    //jogar
-  //} else {
-   // mensagemSair
- // }
-
-    /*for (let c = i; c >= f; c-= p) {
-				res.innerHTML +=`${c}`
-
-				//for (var i = Things.length - 1; i >= 0; i--) {
-				//	Things[i]
-				//}
-			}
-
-/*
-
-    //ir para a funçao jogar
-    //document.write 
-   // else
-        // apresentar a mensagem de até logo e sair do programa
-
-//function jogar(nome, ) {
-    // colocar nome do jogador
-    // iniciar perguntas
-    // Pergunta 1 - Qual é o nome do ator que interpreta Lúcifer na série da Netflix?
-    //a - Tom Ellis, b - Tom Riddle,  3 - Tom Ford
-      //2 - Existem quantos protagonistas na série Friends?
-      // a - 1 , b - 2, c - 6
-      //3 - Em que ano foi lançada a série Game of Thrones?
-      //a - 2010, b - 2008, c - 2020
-
-    //apresentar resultado final - acertos e erros
-    
- // } 
-
- // function mensagemSair {
-  //let xau = window.prompt("Você deseja mesmo sair?") 
- //}
